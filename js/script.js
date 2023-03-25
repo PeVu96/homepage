6646bb3515ac3d09d97185fbdf1f7e11b0275ed2
Patryk Wernerowicz <patryk.wernerowicz1@gmail.com>
2023-03-25 20:59:12 +0100

    arrow functions added to js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-console.log("Witam wszystkich na mojej pierwszej stronie!");
+{
+    const welcome = () => {
+        console.log("Witam wszystkich na mojej pierwszej stronie!");
+    }
 
-let bodyContainer = document.querySelector(".js-body__container");
-let buttonBarca = document.querySelector(".js-button");
-let buttonTheme = document.querySelector(".js-button__theme");
+    const toggleBackground = () => {
+        const bodyContainer = document.querySelector(".js-body__container");
+        const buttonTheme = document.querySelector(".js-button__theme");
 
-buttonBarca.addEventListener("click", () => {
-    bodyContainer.classList.toggle("darkTheme");
-    buttonBarca.classList.toggle("darkThemeButton");
-    buttonTheme.innerText = bodyContainer.classList.contains("darkTheme") ? "barceloński" : "czarno-biały";
-});
+        bodyContainer.classList.toggle("darkTheme");
+        buttonBarca.classList.toggle("darkThemeButton");
+        buttonTheme.innerText = bodyContainer.classList.contains("darkTheme") ? "barceloński" : "czarno-biały";
+    };
+
+    const init = () => {
+        const buttonBarca = document.querySelector(".js-button");
+
+        buttonBarca.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+
+    init();
+}
