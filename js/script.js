2f04cb26abf5177059336fcdf6a1f6f5a4972ffc
Patryk Wernerowicz <patryk.wernerowicz1@gmail.com>
2023-07-20 22:52:20 +0200

    dark theme modified

--- js/script.js
+++ js/script.js
@@ -8,10 +8,21 @@
     const toggleBackground = () => {
         const bodyContainer = document.querySelector(".js-body__container");
         const buttonName = document.querySelector(".js-themeName");
+        const headerTitle = document.querySelector(".js-header__title");
+        const navigation = document.querySelector(".js-navigation__list");
+        const navigationLink = document.querySelector(".js-navigation__link");
+        const sectionHeader = document.querySelector(".js-section__header");
+        const sectionParagraph = document.querySelector(".js-section__paragraph");
 
         bodyContainer.classList.toggle("darkTheme");
         buttonTheme.classList.toggle("darkThemeButton");
-        buttonName.innerText = bodyContainer.classList.contains("darkTheme") ? "barceloński" : "czarno-biały";
+        headerTitle.classList.toggle("darkThemeTitle");
+        navigation.classList.toggle("darkThemeNavigation");
+        navigationLink.classList.toggle("darkThemeNavigationLink");
+        sectionHeader.classList.toggle("darkThemeSectionHeader");
+        sectionParagraph.classList.toggle("darkThemeSectionParagraph");
+
+        buttonName.innerText = bodyContainer.classList.contains("darkTheme") ? "jasny" : "ciemny";
     };
 
     const init = () => {
